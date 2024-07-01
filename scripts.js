13e1035e67f3a71c2281550f852e941cffa4ff77
soni-shashan <lumbhanishashan1510@gmail.com>
2024-07-01 22:46:16 +0530

    added devlopers page

--- scripts.js
+++ scripts.js
@@ -10,3 +10,14 @@ document.getElementById("examResult").href="https://charusat.edu.in:912/Uniexamr
 document.getElementById("examResult").target="_blank";
 document.getElementById("feePay").href="https://charusat.edu.in:912/FeesPaymentApp/";
 document.getElementById("feePay").target="_blank";
+var imageId_shashan = "1K0QhMcN4HoeQxH8wnUTt5SQA8MAk4n4X";
+var LinkedIn_shashan ="https://www.linkedin.com/in/shashan-lumbhani";
+var Instagram_shashan ="https://www.instagram.com/mr.shashan_official";
+var github_shashan ="https://github.com/soni-shashan/";
+document.getElementById("icon_l_shashan").href = LinkedIn_shashan;
+document.getElementById("icon_l_shashan").target="_blank";
+document.getElementById("icon_i_shashan").href = Instagram_shashan;
+document.getElementById("icon_i_shashan").target="_blank";
+document.getElementById("icon_g_shashan").href = github_shashan;
+document.getElementById("icon_g_shashan").target="_blank";
+
