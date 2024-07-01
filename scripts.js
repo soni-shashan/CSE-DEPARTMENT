d534d4e13e859eb5a0f4abb3cf0815f7a83b862c
soni-shashan <lumbhanishashan1510@gmail.com>
2024-07-01 20:22:45 +0530

    added social media link

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById("facebook").href="https://www.facebook.com/people/Cse-Cspit/pfbid02tJNiMmCnzHvjWHKoLsuSUxbYRCSgELqtUGTKPvmL3eLouYnf553nUucaeEJPpLwbl/";
+document.getElementById("instagran").href="https://www.instagram.com/cse_cspit/";
+document.getElementById("linkedin").href="https://www.linkedin.com/in/cspit-cse/";
