a647200d3478bf78fc7f81916cf62e6cfd8435f6
soni-shashan <lumbhanishashan1510@gmail.com>
2024-07-01 20:25:52 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 document.getElementById("facebook").href="https://www.facebook.com/people/Cse-Cspit/pfbid02tJNiMmCnzHvjWHKoLsuSUxbYRCSgELqtUGTKPvmL3eLouYnf553nUucaeEJPpLwbl/";
+document.getElementById("facebook").target="_blank";
 document.getElementById("instagran").href="https://www.instagram.com/cse_cspit/";
+document.getElementById("instagran").target="_blank";
 document.getElementById("linkedin").href="https://www.linkedin.com/in/cspit-cse/";
+document.getElementById("linkedin").target="_blank";
